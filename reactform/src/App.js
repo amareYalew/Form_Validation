a524af84e0f222978d6ed87dcfca826c296d7cad
Amare Yalew <amyalew@mum.edu>
2020-08-17 22:21:16 -0500

    React form validation by formik and yup

---
+++ reactform/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Employee from './Formik'
+import Yap from './Yup'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      <Employee ></Employee>
+      <Yap></Yap>
+    </div>
+  );
+}
+
+export default App;
